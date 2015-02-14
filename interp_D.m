a7cae3ac3d87aa798fe414536b12f241427ed303
David Nagy <dn69@hw.ac.uk>
2015-02-14 16:02:26 +0000

    no message

--- interp_D.m
+++ interp_D.m
@@ -1,6 +1,5 @@
+%Function interpolates between the two variable valy1 and valy2 taking into
+%account the relationship between the other three variables valx1,valxmid
+%and valx2.
 function [value] = interp_D(valx1,valxmid,valx2,valy1,valy2)
 value = valy1 + (valy2-valy1)*((valxmid-valx1)/(valx2-valx1));
-%% SECTION TITLE
-% DESCRIPTIVE TEXT
-
-%% trdfui
